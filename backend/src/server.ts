1957178ed8a9c1c45509f911b0bc0bf15cf79689
vdonoladev <victordonolaf@gmail.com>
2024-07-09 14:12:00 -0300

    :tada: init: finalização da aula 01

---
+++ backend/src/server.ts
@@ -0,0 +1,24 @@
+import cors from "@fastify/cors";
+import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import { createTrip } from "./routes/create-tips";
+
+const app = fastify();
+
+app.register(cors, {
+  origin: "*",
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip);
+app.register(confirmTrip);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server running!");
+});
